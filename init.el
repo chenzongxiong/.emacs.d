ac91dd0abc16a8fe13ec89e9486e00320ddaa7c1
czx陈宗雄 <zxchen@ctrip.com>
2015-09-17 17:08:52 +0800

    modify some configure in linux

--- init.el
+++ init.el
@@ -2,13 +2,9 @@
     (progn
       (require 'cl)
       (load-file "~/.emacs.d/general-settings.el")
+      (load-file "~/.emacs.d/OS-specific.el")
       (load-file "~/.emacs.d/mypackages.el")
       (load-file "~/.emacs.d/packages-settings.el")
-      ;; if the system-type if darwin
-      (when (eq system-type 'darwin)
-        (setq mac-command-modifier nil)
-        (setq mac-option-modifier 'meta)
-        (load-file "~/.emacs.d/third-part-loading.el"))
-      )
-  (message "Please Update the emacs")
-)
+      (load-file "~/.emacs.d/third-part-loading.el"))
+  (message "Please Update the emacs"))
+(put 'erase-buffer 'disabled nil)
