98557160bc28304506b9be4ad35e9aa680adac41
Zongxiong, Chen <czxczf@gmail.com>
2023-09-07 16:24:36 +0200

    omit .DS_Store in dired

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -3,13 +3,19 @@
 ;;; Commentary:
 
 (add-hook 'dired-load-hook #'(lambda ()
-                              (require 'wdired)))
+                               (require 'wdired)
+                               (defconst wdired-allow-to-change-permissions t)
+                               ))
 ;; you can modify the files attributes in dired
-(setq wdired-allow-to-change-permissions t)
-(add-hook 'dired-load-hook #'(lambda ()
-                              (require 'dired-x)))
-(setq dired-omit-mode t)
-(setq dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$|.pyc$|^\\.DS_Store$|^__pycache__$")
+
+(add-hook 'dired-load-hook
+          #'(lambda ()
+              (require 'dired-x)
+              (defconst dired-omit-mode t)
+              (defconst dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$|\\.pyc$\\|\\.DS_Store$\\|__pycache__\\|^\\.git\\|\\.zcompdump.*")
+              ))
+
+;;
 (provide 'init-dired)
 
 ;;; init-dired.el ends here
